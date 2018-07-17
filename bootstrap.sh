0d0aa6eb80f70e28e2ae96830375ab381cf42380
Maxim Korzhenevsky <mk.prozorro@gmail.com>
2018-07-17 14:41:35 +0300

    fix bootstrap script and change web port

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 virtualenv --clear --never-download .
 #./bin/pip install -r requirements.txt
-pip install setuptools==18.3.2 --user
-python bootstrap.py
+/usr/bin/pip install setuptools==18.3.2 --user
+/usr/bin/python bootstrap.py
